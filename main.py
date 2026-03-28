0e902b1c3a96a93747db5f636553d32bbed55fec
Adriana Maria Paternina Paez <adriana-maria.paternina-paez.1@ulaval.ca>
2026-03-28 15:00:23 -0400

    working on main.py

--- main.py
+++ main.py
@@ -24,5 +24,30 @@ def main():
     print("\nNumber of users:")
     print(len(data["users"]))
 
+    print("\nGrouping posts by user...")
+
+    posts_by_user = {}
+
+    for post in data["posts"]:
+        user_id = post["author_id"]
+
+        if user_id not in posts_by_user:
+            posts_by_user[user_id] = []
+
+        posts_by_user[user_id].append(post)
+
+    print("Done grouping!")
+
+    print("\nNumber of users in grouping:")
+    print(len(posts_by_user))
+
+    # exemple user ID and number of posts for that user
+    some_user = list(posts_by_user.keys())[0]
+    print("\nExample user ID:")
+    print(some_user)
+
+    print("Number of posts for this user:")
+    print(len(posts_by_user[some_user]))
+
 if __name__ == "__main__":
     main()
